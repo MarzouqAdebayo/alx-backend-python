7ba6a73751aa5eeaf030ce267826f87fe38b37f9
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-12 07:56:51 +0100

    Compelted Adv task 1

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module '101-safely_get_value.py'"""
+
+T = TypeVar("T")
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Get a value from a dict using a key"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
