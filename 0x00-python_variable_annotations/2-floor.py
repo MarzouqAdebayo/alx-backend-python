d41124d249536fcc3ab373148f13f8f9137aaee9
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-12 05:07:11 +0100

    Comleted task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module '2-float.py' - contains function floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floored value of a float"""
+    return math.floor(n)
