4070dff879f4bb0ef6456ca3c6a76c4b863df092
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-12 05:16:27 +0100

    Fixed task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module '5-sum_list.py' contains function sum_list"""
+import math
 
 
 def sum_list(input_list: list[float]) -> float:
     """Sums a list of floats"""
-    return sum(input_list)
+    return math.fsum(input_list)
