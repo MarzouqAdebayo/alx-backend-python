cad340c52e2ddedc75000a7ddc2f0923eb7b9395
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-12 05:29:44 +0100

    Completed task 6

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module '5-sum_list.py' contains function sum_list"""
-from typing import List
+from typing import List, Union
 
 
 def sum_list(input_list: List[float]) -> float:
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module 6-sum_mixed_list.py"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sums a mixed list of int and float"""
+    return sum(mxd_lst)
