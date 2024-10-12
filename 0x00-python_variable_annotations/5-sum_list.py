55018efa496334db8622bbd78998ad641d6bbe12
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-12 05:18:19 +0100

    Trying again

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Module '5-sum_list.py' contains function sum_list"""
-import math
 
 
 def sum_list(input_list: list[float]) -> float:
     """Sums a list of floats"""
-    return math.fsum(input_list)
+    return sum(input_list)
