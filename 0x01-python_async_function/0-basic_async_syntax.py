89953bb7f001f724a5623881ecc584e77a2914e1
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-16 02:12:00 +0100

    Completed task 0 and added README.md

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module '0-basic_async_syntax' contains function wait_random"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay):
+    """async function to delay for some time"""
+    delay_time = random.uniform(max_delay)
+    await asyncio.seep(delay_time)
+    return delay_time
