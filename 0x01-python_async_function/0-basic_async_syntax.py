b6dcdeff5a8003a89791efb5e3ceba945c09b2a5
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-16 02:15:13 +0100

    Fixed typo in task 0. Changed from asyncio.seep to asyncio.sleep

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,5 +7,5 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """async function to delay for some time"""
     delay_time = random.uniform(max_delay)
-    await asyncio.seep(delay_time)
+    await asyncio.sleep(delay_time)
     return delay_time
