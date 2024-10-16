3615de4d2b1733be3e4dfbaff0d80dccfb493897
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-16 02:19:17 +0100

    Fixed error in random.uniform function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """async function to delay for some time"""
-    delay_time = random.uniform(max_delay)
+    delay_time = random.uniform(0, max_delay)
     await asyncio.sleep(delay_time)
     return delay_time
