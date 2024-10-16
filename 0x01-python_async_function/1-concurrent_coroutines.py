5fc876d5966464daf06222a076eb68408f1f5f9b
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-16 02:34:02 +0100

    Changed function to return sorted list instead

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,4 +9,4 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """executes multiple coroutines at the same time with async"""
     result = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
-    return result
+    return sorted(result)
