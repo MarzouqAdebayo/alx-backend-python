07d9baadd2890ff0aab01baa816e6421f28f47a5
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-16 05:45:48 +0100

    Completed ttask 2 for test

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module '2-measure_runtime.py' contains measure_time """
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the runtime of coroutines"""
+    start = time.perf_counter()
+    _ = asyncio.run(async_comprehension())
+    end = time.perf_counter() - start
+    return end / n
