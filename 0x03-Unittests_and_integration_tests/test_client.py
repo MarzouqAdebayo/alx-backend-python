ab4aa432f1ed598f19f41ecca77f089ed9e2d1d5
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-05 05:13:37 +0100

    :sparkles: Completed task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,25 @@
 #!#!/usr/bin/env python3
 """"""
 import unittest
+from typing import Dict
+from unittest.mock import MagicMock, patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """"""
-    pass
+
+    @parameterized.expand(
+        [
+            ("google", {"login": "google"}),
+            ("abc", {"login": "abc"}),
+        ]
+    )
+    @patch("client.get_json")
+    def test_org(self, org: str, response: Dict, mocked_fn: MagicMock):
+        mocked_fn.return_value = lambda: response
+        org_client = GithubOrgClient(org)
+        self.assertEqual(org_client.org(), response)
+        mocked_fn.assert_called_once_with("https://api.github.com/orgs/{}".format(org))
